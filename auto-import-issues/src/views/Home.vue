35454b5bb434d12b78d64fcba6cb7be71519bb8c
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-03-25 00:17:23 +0800

    feat: update

---
+++ auto-import-issues/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>{{ test }}</div>
+  <Hello />
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+import Hello from '../components/Hello.vue'
+
+const test = ref(0)
+
+</script>
